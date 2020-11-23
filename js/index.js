a5566544bf333976c7a22ed9c6b8fd4be678d54c
cutecat8110 <cutecat8110@gmail.com>
2020-11-23 16:09:12 +0800

    navCloser

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-$(".humburger").click(function () {
-    $(this).parent().parent("body").toggleClass('nav-open')
+$("header").click(function () {
+    if (!$("body").hasClass('nav-open')) {
+        $("body").addClass('nav-open')
+    }
+})
+
+$(".navCloser").click(function () {
+    if ($("body").hasClass('nav-open')) {
+        $("body").removeClass('nav-open')
+        event.stopPropagation()
+    }
 })
 
 $("#container").click(function () {
-    if ($('body').hasClass('nav-open')) {
-        $('body').removeClass('nav-open')
+    if ($("body").hasClass('nav-open')) {
+        $("body").removeClass('nav-open')
     }
 })
