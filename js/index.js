420578362110e230ddc7d31da29728b31d400ede
cutecat8110 <cutecat8110@gmail.com>
2021-01-30 13:02:50 +0800

    menu

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $("header").click(function () {
     }
 })
 
-$(".navCloser").click(function () {
+$(".navCloser").click(function (event) {
     if ($("body").hasClass('nav-open')) {
         $("body").removeClass('nav-open')
         event.stopPropagation()
@@ -16,3 +16,37 @@ $("#container").click(function () {
         $("body").removeClass('nav-open')
     }
 })
+
+// Swiper
+
+
+var swiper = new Swiper('.swiper-container1', {   // newsLetter
+    slidesPerView: 3,
+    spaceBetween: 36,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {  //menu
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    }
+});
